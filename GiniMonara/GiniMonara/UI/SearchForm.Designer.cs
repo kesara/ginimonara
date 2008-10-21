bdbae1b4765d5e5717ff033b68d1499b2c90dd09
k3z <k3z@b7f1a231-1557-4b73-873a-c12f13da6a9a>
2008-10-21 17:07:27 +0000

    Search Feature and other minor enhancements.

---
+++ GiniMonara/GiniMonara/UI/SearchForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace GiniMonara.UI
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ribbonButtonSearch = new mRibbon.RibbonButton();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.dataGridViewResults = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbonButtonSearch
+            // 
+            this.ribbonButtonSearch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ribbonButtonSearch.Image = global::GiniMonara.Properties.Resources.search;
+            this.ribbonButtonSearch.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ribbonButtonSearch.IsFlat = true;
+            this.ribbonButtonSearch.IsPressed = false;
+            this.ribbonButtonSearch.Location = new System.Drawing.Point(451, 9);
+            this.ribbonButtonSearch.Margin = new System.Windows.Forms.Padding(0);
+            this.ribbonButtonSearch.Name = "ribbonButtonSearch";
+            this.ribbonButtonSearch.Padding = new System.Windows.Forms.Padding(2);
+            this.ribbonButtonSearch.Size = new System.Drawing.Size(81, 35);
+            this.ribbonButtonSearch.TabIndex = 6;
+            this.ribbonButtonSearch.Text = "Search";
+            this.ribbonButtonSearch.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.ribbonButtonSearch.Click += new System.EventHandler(this.ribbonButtonSearch_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(12, 17);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(436, 20);
+            this.textBoxSearch.TabIndex = 5;
+            // 
+            // dataGridViewResults
+            // 
+            this.dataGridViewResults.AllowUserToAddRows = false;
+            this.dataGridViewResults.AllowUserToDeleteRows = false;
+            this.dataGridViewResults.AllowUserToOrderColumns = true;
+            this.dataGridViewResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResults.Location = new System.Drawing.Point(12, 47);
+            this.dataGridViewResults.Name = "dataGridViewResults";
+            this.dataGridViewResults.ReadOnly = true;
+            this.dataGridViewResults.Size = new System.Drawing.Size(517, 379);
+            this.dataGridViewResults.TabIndex = 7;
+            this.dataGridViewResults.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewResults_CellContentClick);
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 438);
+            this.Controls.Add(this.dataGridViewResults);
+            this.Controls.Add(this.ribbonButtonSearch);
+            this.Controls.Add(this.textBoxSearch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "SearchForm";
+            this.Text = "GiniMonara Search";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SearchForm_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private mRibbon.RibbonButton ribbonButtonSearch;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.DataGridView dataGridViewResults;
+
+    }
+}
---
+++ GiniMonara/GiniMonara/UI/SearchForm.cs
@@ -0,0 +1,92 @@
+﻿using GiniMonara.MetaData;
+using GiniMonara.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+/*
+ * SearchForm - GiniMonara Search User Interface
+ * Developer: Kesara Nanayakkara Rathnayake < kesara@bcs.org >
+ * Copyright (C) 2008 GiniMonara Team
+ * 
+ * This file is part of GiniMonara.
+ * 
+ * GiniMonara is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ * 
+ * GiniMonara is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GiniMonara.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+namespace GiniMonara.UI
+{
+    public partial class SearchForm : Form
+    {
+        private MainForm parentUI;
+        private DataTable searchResults;
+
+        public SearchForm(MainForm caller)
+        {
+            parentUI = caller;
+            InitializeComponent();
+        }
+
+        private void ribbonButtonSearch_Click(object sender, EventArgs e)
+        {
+            searchResults = new DataTable();
+            searchResults.Columns.Add("Data");
+            searchResults.Columns.Add("File Name");
+            searchResults.Columns.Add("Type");
+
+            string[] metaDataFiles = Directory.GetFiles(ApplicationUtility.metaDataDirectory, "*.xml");
+            foreach (string metaDataFile in metaDataFiles)
+            {
+                TagList tagList = new TagList();
+                tagList.load(metaDataFile);
+
+                var tags = tagList.Where(d => d.data.ToLower().Contains(textBoxSearch.Text.ToLower())).Select(t => t);
+
+                foreach (gTag tag in tags)
+                {
+                    DataRow searchRecord = searchResults.NewRow();
+                    searchRecord["Data"] = tag.data;
+                    searchRecord["File Name"] = tagList.Where(t => t.name == "fileName").Select(v => v.data).FirstOrDefault();
+                    searchRecord["Type"] = tagList.Where(t => t.name == "mediaType").Select(v => v.data).FirstOrDefault();
+                    searchResults.Rows.Add(searchRecord);
+                }
+            }
+
+            dataGridViewResults.DataSource = searchResults;
+        }
+
+        private void SearchForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            parentUI.reloadCategories();
+            parentUI.Enabled = true;
+        }
+
+        private void dataGridViewResults_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (searchResults.Rows[e.RowIndex]["Type"].ToString() == "image")
+            {
+                parentUI.openImage(searchResults.Rows[e.RowIndex]["File Name"].ToString());
+                this.Close();
+            }
+            else if (searchResults.Rows[e.RowIndex]["Type"].ToString() == "video")
+            {
+                parentUI.openVideo(searchResults.Rows[e.RowIndex]["File Name"].ToString());
+                this.Close();
+            }
+        }
+    }
+}
